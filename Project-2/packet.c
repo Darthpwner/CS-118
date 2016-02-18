a1ff5bcbc9876e9875effdcd7e3f680c290a2f84
kfrankc <frankchen1698@gmail.com>
2016-02-18 14:36:34 -0800

    updated packet and server.c

---
+++ Project-2/packet.c
@@ -0,0 +1,18 @@
+/* Struct Data Structure for Packets
+   - type: indicate type of packet
+   		0 - request
+   		1 - ACK
+   		2 - data
+   		3 - teardown
+   - sequence_no: denotes the current packet's sequence number
+   - length: length of the data
+   - data: the actual data  */
+
+#define DATA_SIZE 1024
+
+struct packet {
+	int type;
+	int sequence_no;
+	int length;
+	char data[DATA_SIZE];
+}
--- Project-2/server.c
+++ Project-2/server.c
@@ -1,4 +1,30 @@
-//receiver.c
+/* server 
+   ------
+   -establish a connection with the client 
+   -send packets to it
+   -receives ACK
+   -retransmits packets for which negative acknowledgement is received
+   
+   We use UDP connection for process */
 
 #include <stdio.h>
-#include <sys/types.h>
+#include <sys/types.h>   // definitions of a number of data types used in socket.h and netinet/in.h
+#include <sys/socket.h>  // definitions of structures needed for sockets, e.g. sockaddr
+#include <netinet/in.h>  // constants and structures needed for internet domain addresses, e.g. sockaddr_in
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/wait.h>	/* for the waitpid() system call */
+#include <signal.h>	/* signal name macros, and the kill() prototype */
+
+/* additionally added libraries */
+#include <time.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <arpa/inet.h> /* need this for inet_ntoa */
+
+#include "packet.c"
+
+#define WINDOW_SIZE 5
+#define TIMEOUT 5000
+
+struct packet WINDOW[WINDOW_SIZE];
